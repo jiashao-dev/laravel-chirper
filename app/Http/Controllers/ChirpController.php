88d3f454cbb8f4b913f27e03000a6f1459498eab
Jia Shao <laujiashao@hotmail.com>
2023-01-02 11:43:45 +0800

    Update Chirp View

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -20,15 +20,15 @@ class ChirpController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Inertia\Response
-     */
-    public function create()
-    {
-        //
-    }
+    // /**
+    //  * Show the form for creating a new resource.
+    //  *
+    //  * @return \Inertia\Response
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
 
     /**
      * Store a newly created resource in storage.
@@ -47,27 +47,27 @@ class ChirpController extends Controller
         return redirect(route('chirps.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Inertia\Response
-     */
-    public function show(Chirp $chirp)
-    {
-        //
-    }
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  \App\Models\Chirp  $chirp
+    //  * @return \Inertia\Response
+    //  */
+    // public function show(Chirp $chirp)
+    // {
+    //     //
+    // }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Chirp  $chirp
-     * @return \Inertia\Response
-     */
-    public function edit(Chirp $chirp)
-    {
-        //
-    }
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  *
+    //  * @param  \App\Models\Chirp  $chirp
+    //  * @return \Inertia\Response
+    //  */
+    // public function edit(Chirp $chirp)
+    // {
+        
+    // }
 
     /**
      * Update the specified resource in storage.
